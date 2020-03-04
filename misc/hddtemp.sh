4a66322c1b2adeaf7c801aa146b811c82383be4b
Pawel Troka <pawel.troka@outlook.com>
2020-03-04 03:00:35 +0100

    add simple script to expose on certain ports hdd temperatures

---
+++ misc/hddtemp.sh
@@ -0,0 +1,8 @@
+sudo hddtemp -d -p 11232 /dev/sdb
+sudo hddtemp -d -p 11233 /dev/sdc
+sudo hddtemp -d -p 11234 /dev/sdd
+sudo hddtemp -d -p 11235 /dev/sde
+sudo hddtemp -d -p 11236 /dev/sdf
+sudo hddtemp -d -p 11237 /dev/sdg
+sudo hddtemp -d -p 11238 /dev/sdh
+sudo hddtemp -d -p 11239 /dev/sdi
